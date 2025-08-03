be9d76fcbf418071e4f911843f633bc5bb239e38
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2025-08-03 23:15:09 +0800

    v2.6.3
    v2.6.3

--- x-ui.sh
+++ x-ui.sh
@@ -308,6 +308,56 @@ check_config() {
         show_menu
     fi
     echo -e "${info}${plain}"
+    echo ""
+    
+    # 获取 IPv4 和 IPv6 地址
+    v4=$(curl -s4m8 http://ip.sb -k)
+    v6=$(curl -s6m8 http://ip.sb -k)
+    local existing_webBasePath=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'webBasePath（访问路径）: .+' | awk '{print $2}') 
+    local existing_port=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'port（端口号）: .+' | awk '{print $2}') 
+    local existing_cert=$(/usr/local/x-ui/x-ui setting -getCert true | grep -Eo 'cert: .+' | awk '{print $2}')
+    local existing_key=$(/usr/local/x-ui/x-ui setting -getCert true | grep -Eo 'key: .+' | awk '{print $2}')
+
+    if [[ -n "$existing_cert" && -n "$existing_key" ]]; then
+        echo -e "${green}面板已安装证书采用SSL保护${plain}"
+        echo ""
+        echo -e "${green}登录访问面板URL: https://你的域名:${existing_port}${existing_webBasePath}${plain}" 
+    fi
+    echo ""
+    if [[ -z "$existing_cert" && -z "$existing_key" ]]; then
+        echo -e "${red}警告：未找到证书和密钥，面板不安全！${plain}"
+        echo ""
+        echo -e "${green}------->>>>请按照下述方法设置〔ssh转发〕<<<<-------${plain}"
+        echo ""
+
+        # 检查 IP 并输出相应的 SSH 和浏览器访问信息
+        if [[ -z $v4 ]]; then
+            echo -e "${green}1、本地电脑客户端转发命令：${plain} ${blue}ssh  -L [::]:15208:127.0.0.1:${existing_port}${blue} root@[$v6]${plain}"
+            echo ""
+            echo -e "${green}2、请在本地打开终端，成功输入服务器的〔root密码〕用以上命令进行转发${plain}"
+            echo ""
+            echo -e "${green}3、请在浏览器地址栏复制${plain} ${blue}[::1]:15208${existing_webBasePath}${plain} ${green}进入〔3X-UI〕登录界面"
+            echo ""
+            echo -e "${red}注意：若不使用〔ssh转发〕请为3X-UI面板配置安装证书再行登录管理后台${plain}"
+        elif [[ -n $v4 && -n $v6 ]]; then
+            echo -e "${green}1、本地电脑客户端转发命令：${plain} ${blue}ssh -L 15208:127.0.0.1:${existing_port}${blue} root@$v4${plain} ${yellow}或者 ${blue}ssh  -L [::]:15208:127.0.0.1:${existing_port}${blue} root@[$v6]${plain}"
+            echo ""
+            echo -e "${green}2、请在本地打开终端，成功输入服务器的〔root密码〕用以上命令进行转发${plain}"
+            echo ""
+            echo -e "${green}3、请在浏览器地址栏复制${plain} ${blue}127.0.0.1:15208${existing_webBasePath}${plain} ${yellow}或者${plain} ${blue}[::1]:15208${existing_webBasePath}${plain} ${green}进入〔3X-UI〕登录界面"
+            echo ""
+            echo -e "${red}注意：若不使用〔ssh转发〕请为3X-UI面板配置安装证书再行登录管理后台${plain}"
+        else
+            echo -e "${green}1、本地电脑客户端转发命令：${plain} ${blue}ssh -L 15208:127.0.0.1:${existing_port}${blue} root@$v4${plain}"
+            echo ""
+            echo -e "${green}2、请在本地打开终端，成功输入服务器的〔root密码〕用以上命令进行转发${plain}"
+            echo ""
+            echo -e "${green}3、请在浏览器地址栏复制${plain} ${blue}127.0.0.1:15208${existing_webBasePath}${plain} ${green}进入〔3X-UI〕登录界面"
+            echo ""
+            echo -e "${red}注意：若不使用〔ssh转发〕请为3X-UI面板配置安装证书再行登录管理后台${plain}"
+            echo ""
+        fi
+    fi
 }
 
 set_port() {
