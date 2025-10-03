0658a7ed2a0b652f760ef0c5ed8e402ea8926f8d
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-10-04 00:19:24 +0800

    Update global.go

--- web/global/global.go
+++ web/global/global.go
@@ -4,32 +4,13 @@ import (
 	"context"
 	_ "unsafe"
 
-	"x-ui/database/model"
-	
 	"github.com/robfig/cron/v3"
 )
-
-type TelegramService interface {
-	SendMessage(msg string) error
-	SendSubconverterSuccess()
-	IsRunning() bool
-    // 【中文注释】: 这个方法是在 inbound.go 中被调用的，所以也必须包含在接口定义里。
-	SendOneClickConfig(ib *model.Inbound, inFromPanel bool) error
-}
-
-type TgBotInterface interface {
-	SendMessage(msg string) error
-	SendSubconverterSuccess()
-	IsRunning() bool
-}
 
 var (
 	webServer WebServer
 	subServer SubServer
-	// 定义全局变量  TelegramService / Tgbot 的接口方法
-	TgBot TelegramService
 )
-
 
 type WebServer interface {
 	GetCron() *cron.Cron
@@ -54,9 +35,4 @@ func SetSubServer(s SubServer) {
 
 func GetSubServer() SubServer {
 	return subServer
-}
-
-// 〔中文注释〕: 设置 / 获取全局 TgBot 引用（由 main 或 service 在启动时注入）。
-func SetTgBot(t TelegramService) {
-	TgBot = t
 }
